f3bcc5de3b3684fdfae5898bacf3390bb33e5098
Unknown <gm21007@ues.edu.sv>
2024-06-14 01:54:24 -0600

    Modulo usuarios casi terminado
    Se puede crear, editar, ver y eliminar usuarios.
    Solo faltan detalles esteticos

--- SistemaEncomiendas/aplicaciones/moduloUsuarios/urls.py
+++ SistemaEncomiendas/aplicaciones/moduloUsuarios/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
-
+app_name = 'moduloUsuarios'
 urlpatterns = [
     path('',views.crudUsuarios, name="users"),
-    path('agregar/',views.agregarUsuario, name="newuser"),
-    path('ver/',views.verUsuario, name="checkuser"),
+    path('agregar/',views.newuser, name="agregar"),
+    path('create/',views.agregarUsuario, name="newuser"),
+    path('ver/<int:pk>/',views.verUsuario, name="verUsuario"),
     path('modificar/',views.modUsuario, name="moduser"),
+    path('eliminar/<int:pk>/', views.eliminar_usuario, name='eliminar_usuario'),
+
 ]
