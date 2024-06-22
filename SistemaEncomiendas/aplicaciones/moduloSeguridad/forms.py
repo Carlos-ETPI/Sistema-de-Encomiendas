3ab6d3a9b0170f24a9f70a981366c1716edcaa45
Carlos-ETPI <carlos.anime.000@gmail.com>
2024-06-22 12:51:11 -0600

    agrego validaciones

--- SistemaEncomiendas/aplicaciones/moduloSeguridad/forms.py
+++ SistemaEncomiendas/aplicaciones/moduloSeguridad/forms.py
@@ -9,7 +9,7 @@ class LoginForm(forms.Form):
         label='Nombre de usuario',
         max_length=50,
         required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control form-control-lg', 'autocomplete': 'on'}),
+        widget=forms.TextInput(attrs={'class': 'form-control form-control-lg ', 'autocomplete': 'on'}),
         error_messages={
             'required': 'Por favor ingrese su nombre de usuario.',
             'max_length': 'El nombre de usuario no puede exceder los 50 caracteres.'
@@ -17,7 +17,7 @@ class LoginForm(forms.Form):
     )
     password = forms.CharField(
         label='Contraseña',
-        max_length=100,
+        max_length=10,
         required=True,
         widget=forms.PasswordInput(attrs={'class': 'form-control form-control-lg'}),
         error_messages={
@@ -30,20 +30,37 @@ class LoginForm(forms.Form):
     
 
 class ChangePasswordForm(PasswordChangeForm):
+    error_messages = {
+        'password_incorrect': ("Tu contraseña antigua es incorrecta. Por favor, intenta de nuevo."),
+        'password_mismatch': ("Las contraseñas no coinciden."),
+    }
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['old_password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Contraseña Actual'},
-        error_messages={
-            'required': 'Por favor ingrese su contraseña Actual.',
-            'max_length': 'La contraseña no puede exceder los 100 caracteres.'
+        
+        self.fields['old_password'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Contraseña Actual'
         })
-        self.fields['new_password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Nueva Contraseña'},
-        error_messages={
-            'required': 'Por favor ingrese su nueva contraseña.',
-            'max_length': 'La contraseña no puede exceder los 100 caracteres.'
+        self.fields['new_password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Nueva Contraseña'
         })
-        self.fields['new_password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirmar Nueva Contraseña'},
-        error_messages={
-            'required': 'Por favor ingrese su nueva contraseña(Confirmacion).',
-            'max_length': 'La contraseña no puede exceder los 100 caracteres.'
+        self.fields['new_password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Confirmar Nueva Contraseña'
         })
+        
+        self.fields['old_password'].error_messages = {
+            'required': ('Por favor ingrese su contraseña actual.'),
+            'max_length': ('La contraseña no puede exceder los 100 caracteres.')
+        }
+        
+        self.fields['new_password1'].error_messages = {
+            'required': ('Por favor ingrese su nueva contraseña.'),
+            'max_length': ('La contraseña no puede exceder los 100 caracteres.')
+        }
+        
+        self.fields['new_password2'].error_messages = {
+            'required': ('Por favor ingrese su confirmación de contraseña.'),
+            'max_length': ('La contraseña no puede exceder los 100 caracteres.')
+        }
--- SistemaEncomiendas/static/js/ValidacionNoVacio.js
+++ SistemaEncomiendas/static/js/ValidacionNoVacio.js
@@ -1,3 +1,24 @@
 setTimeout(function() {
     document.querySelector('.alert').classList.add('d-none');
 }, 5000);
+
+
+(function () {
+    'use strict';
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation');
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+
+                form.classList.add('was-validated');
+            }, false);
+        });
+})();
