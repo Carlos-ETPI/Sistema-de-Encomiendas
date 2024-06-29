17f45d53d0065767b5a9e5b95b15ac6108db4952
Carlos-ETPI <carlos.anime.000@gmail.com>
2024-06-29 10:16:58 -0600

    agrgue nuevos mensajes de validacion

--- SistemaEncomiendas/static/js/ValidacionNoVacio.js
+++ SistemaEncomiendas/static/js/ValidacionNoVacio.js
@@ -1,15 +1,21 @@
+//Se utiliza para medir el tiempo antes que desaparezca el mensaje de error
 setTimeout(function() {
-    document.querySelector('.alert').classList.add('d-none');
+    document.querySelector('.toast').classList.add('d-none');
 }, 5000);
 
+//Se utiliza para mostrar mensajes de error
+$(document).ready(function() {
+    $('.toast').toast('show');
+});
 
+//se utiliza para la validacion de los campos usando Bootstrap
 (function () {
     'use strict';
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    
     var forms = document.querySelectorAll('.needs-validation');
 
-    // Loop over them and prevent submission
+    
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
             form.addEventListener('submit', function (event) {
